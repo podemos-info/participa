a56f06ea88a47161ce10ea57ffd969cdfefccd6c
Andrés Pereira <apereira@alabs.es>
2014-09-16 13:09:10 +0200

    deploy: unicorn (prod/stag)

---
+++ config/unicorn/staging.rb
@@ -0,0 +1,7 @@
+working_directory "/var/www/beta.juntos.podemos.info/current"
+pid "/var/www/beta.juntos.podemos.info/shared/pids/unicorn.pid"
+stderr_path "/var/www/beta.juntos.podemos.info/shared/log/unicorn.log"
+stdout_path "/var/www/beta.juntos.podemos.info/shared/log/unicorn.log"
+listen "/tmp/unicorn.beta.juntos.podemos.info.sock"
+worker_processes 4
+timeout 30
