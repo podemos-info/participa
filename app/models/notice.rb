a1596eb3949923b29a2c9ffa8caf7022df6b698f
Andrés Pereira <apereira@alabs.es>
2014-09-17 12:29:17 +0200

    notices: fixes and validation presence

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,2 +1,3 @@
 class Notice < ActiveRecord::Base
+  validates :title, :body, presence: true
 end
--- test/models/notice_test.rb
+++ test/models/notice_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class NoticeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should validate presence:true" do
+    n = Notice.new
+    n.valid?
+    assert(n.errors[:title].include? "no puede estar en blanco")
+    assert(n.errors[:body].include? "no puede estar en blanco")
+  end
+
 end
