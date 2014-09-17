4c82cf05d4e895125e40831b96c97d29ef69c699
Andrés Pereira <apereira@alabs.es>
2014-09-17 11:03:22 +0200

    admin: dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,23 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+
+    columns do
+      column do
+        panel "Últimos usuarios dados de alta" do
+          ul do
+            User.limit(15).map do |user|
+              li link_to(user.full_name, admin_user_path(user)) + "- #{user.created_at}"
+            end
+          end
+        end
+      end
+
+      column do
+        panel "Notificaciones" do
+          link_to("Enviar notificación a todos", new_admin_notice_path, class: "button") 
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
