92fbbfc9166e128c636196a40257799eadf938f1
Andrés Pereira <apereira@alabs.es>
2014-09-23 10:38:48 +0200

    mobile: permit iframes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
 
   before_filter :set_phone
   before_action :set_locale
+  before_filter :allow_iframe_requests
+
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options')
+  end
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
