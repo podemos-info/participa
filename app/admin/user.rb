077a3dd9f368d23299812ec6611e026f2f4a0f12
Andrés Pereira <apereira@alabs.es>
2014-10-01 13:23:21 +0200

    registration: admin subregion form province

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -16,22 +16,6 @@ ActiveAdmin.register User do
   filter :sign_in_count
   filter :created_at
 
-  form do |f|
-    f.inputs "Admin Details" do
-      f.input :email
-      f.input :first_name
-      f.input :last_name
-      f.input :document_type
-      f.input :document_vatid
-      f.input :born_at
-      f.input :address
-      f.input :town
-      f.input :postal_code
-      f.input :province
-      f.input :country
-      f.input :wants_newsletter
-    end
-    f.actions
-  end
+  form partial: "form"
 
 end
