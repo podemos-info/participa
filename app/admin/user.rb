c57010f57bfa2a65f7ab04d994848e60938f4e79
Andrés Pereira <apereira@alabs.es>
2014-10-02 11:25:50 +0200

    registrations: .document_type_name, .country_name and .province_name methods and tests

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -11,6 +11,29 @@ ActiveAdmin.register User do
     actions
   end
 
+  show do 
+    attributes_table do
+      row :first_name
+      row :last_name
+      row :document_type do 
+        user.document_type_name
+      end
+      row :document_vatid
+      row :born_at
+      row :email
+      row :town
+      row :postal_code
+      row :province do
+        user.province_name
+      end
+      row :country do
+        user.country_name
+      end
+      row :wants_newsletter
+    end
+    active_admin_comments
+  end
+
   filter :email
   filter :current_sign_in_at
   filter :sign_in_count
