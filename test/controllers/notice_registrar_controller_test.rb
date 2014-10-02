d18bc5b154f6e9d3ebd280f4322b0630412fb6f9
Andrés Pereira <apereira@alabs.es>
2014-10-02 15:03:51 +0200

    notices: elections and notices admin flow

--- test/controllers/notice_registrar_controller_test.rb
+++ test/controllers/notice_registrar_controller_test.rb
@@ -1,9 +1,18 @@
 require 'test_helper'
 
 class NoticeRegistrarControllerTest < ActionController::TestCase
-  test "should get registrate" do
-    get :registrate
-    assert_response :success
+
+  test "should post to registrate" do
+    id = "1111111"
+    post :registrate, notice_registrar: {registration_id: id}
+    assert_equal id, NoticeRegistrar.find_by_registration_id(id).registration_id
+    assert_equal 1, NoticeRegistrar.all.count
+    post :registrate, notice_registrar: {registration_id: "2222222"}
+    assert_equal 2, NoticeRegistrar.all.count
+    assert_response 201
+    post :registrate, notice_registrar: {registration_id: "2222222"}
+    assert_equal 2, NoticeRegistrar.all.count
+    assert_response 201
   end
 
 end
