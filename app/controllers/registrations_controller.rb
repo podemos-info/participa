6c25a0371d8a4fcc8e82d06565cc7fcde36f2ed2
Andrés Pereira <apereira@alabs.es>
2014-10-07 11:43:20 +0200

    lopd: registrations simple_captcha

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,14 +4,25 @@ class RegistrationsController < Devise::RegistrationsController
     render partial: 'subregion_select'
   end
 
+  def create
+    if simple_captcha_valid? 
+      super
+    else
+      build_resource(sign_up_params)
+      clean_up_passwords(resource)
+      flash.now[:alert] = t('simple_captcha.invalid')
+      render :new
+    end
+  end
+
   # http://www.jacopretorius.net/2014/03/adding-custom-fields-to-your-devise-user-model-in-rails-4.html
   private
- 
+
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :born_at, :wants_newsletter, :document_type, :document_vatid, :terms_of_service, :address, :town, :province, :postal_code, :country)
 
   end
- 
+
   def account_update_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :born_at, :wants_newsletter, :document_type, :document_vatid, :address, :town, :province, :postal_code, :country)
 
