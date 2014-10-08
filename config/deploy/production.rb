75ad75b80723db8c49d4a49aa4fa91cdcd8964a0
Andrés Pereira <apereira@alabs.es>
2014-10-08 12:17:00 +0200

    deploy: production for unicorn/capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,23 @@ role :web, %w{capistrano@juntos.podemos.info}
 role :db,  %w{capistrano@juntos.podemos.info}
 
 set :branch, :production
-set :deploy_to, '/var/www/juntos.podemos.info'
+set :deploy_to, '/var/www/participa.podemos.info'
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :start do
+    on roles(:app) do
+      execute "/etc/init.d/unicorn_production start"
+    end
+  end
+  task :stop do
+    on roles(:app) do
+      execute "/etc/init.d/unicorn_production stop"
+    end
+  end
+  task :restart do
+    on roles(:app) do
+      execute "/etc/init.d/unicorn_production restart"
+    end
+  end
+end
