086c6c929ab5c6d7744febc43420d3d6f52affea
Andrés Pereira <apereira@alabs.es>
2014-10-13 19:42:16 +0200

    legacy: podemos import  fixes

--- lib/podemos_import.rb
+++ lib/podemos_import.rb
@@ -42,16 +42,20 @@ class PodemosImport
     # intentamos  convertir por postal_code, si es de españa y el CP corresponde con alguno devolvemos la provincia.
     # si no encuentra la provincia basada en el postal_code + country, devuelve la provincia que ya esta puesta
     # normalizamos provincias
-    all_pcs = {"01"=>"vi", "02"=>"ab", "03"=>"a", "04"=>"al", "05"=>"av", "06"=>"ba", "07"=>"bi", "08"=>"b", "09"=>"bu", "10"=>"cc", "11"=>"ca", "12"=>"cs", "13"=>"cr", "14"=>"co", "15"=>"c", "16"=>"cu", "17"=>"gi", "18"=>"gr", "19"=>"gu", "20"=>"ss", "21"=>"h", "22"=>"hu", "23"=>"j", "24"=>"le", "25"=>"l", "26"=>"lo", "27"=>"lu", "28"=>"m", "29"=>"ma", "30"=>"mu", "31"=>"na", "32"=>"or", "33"=>"o", "34"=>"p", "35"=>"gc", "36"=>"po", "37"=>"sa", "38"=>"tf", "39"=>"s", "40"=>"sg", "41"=>"se", "42"=>"so", "43"=>"t", "44"=>"te", "45"=>"to", "46"=>"v", "47"=>"va", "48"=>"bi", "49"=>"z", "50"=>"za", "51"=>"ce", "52"=>"ml"}
+    all_pcs = {"01"=>"VI", "02"=>"AB", "03"=>"A", "04"=>"AL", "05"=>"AV", "06"=>"BA", "07"=>"BI", "08"=>"B", "09"=>"BU", "10"=>"CC", "11"=>"CA", "12"=>"CS", "13"=>"CR", "14"=>"CO", "15"=>"C", "16"=>"CU", "17"=>"GI", "18"=>"GR", "19"=>"GU", "20"=>"SS", "21"=>"H", "22"=>"HU", "23"=>"J", "24"=>"LE", "25"=>"L", "26"=>"LO", "27"=>"LU", "28"=>"M", "29"=>"MA", "30"=>"MU", "31"=>"NA", "32"=>"OR", "33"=>"O", "34"=>"P", "35"=>"GC", "36"=>"PO", "37"=>"SA", "38"=>"TF", "39"=>"S", "40"=>"SG", "41"=>"SE", "42"=>"SO", "43"=>"T", "44"=>"TE", "45"=>"TO", "46"=>"V", "47"=>"VA", "48"=>"BI", "49"=>"Z", "50"=>"ZA", "51"=>"CE", "52"=>"ML"}
     if ["España", "Spain"].include? country
-      return all_pcs[postal_code[0..1]] 
+      prov = all_pcs[postal_code[0..1]] 
+      if prov.nil? 
+        return province 
+      else
+        return prov
+      end 
     else
       return province
     end
   end
 
   def self.invalid_record(u, row)
-    debugger
     if u.errors[:email].include? "Ya estas registrado con tu correo electrónico. Prueba a iniciar sesión o a pedir que te recordemos la contraseña."
       logger = Logger.new("#{Rails.root}/log/users_email.log")
       logger.info "*" * 10
@@ -69,40 +73,40 @@ class PodemosImport
   def self.process_row(row)
     now = DateTime.now
     u = User.new
-    u.first_name = row[1][1]
-    u.last_name = row[2][1]
-    u.document_vatid = row[4][1]
-    u.document_type = PodemosImport.convert_document_type(row[3][1], u.document_vatid)
+    u.first_name = row[0][1]
+    u.last_name = row[1][1]
+    u.document_vatid =  row[3][1] == "" ? row[4][1] : row[3][1]
+    u.document_type = PodemosImport.convert_document_type(row[2][1], u.document_vatid)
     # legacy: al principio no se preguntaba fecha de nacimiento
-    unless row[6][1] == ""
-      u.born_at = Date.parse row[6][1] # 1943-10-15 
+    unless row[5][1] == "" or row[5][1].nil?
+      u.born_at = Date.parse row[5][1] # 1943-10-15
     end
-    u.email = row[7][1]
-    u.phone = row[8][1].sub('+','00')
-    u.sms_confirmation_token = row[9][1]
-    u.address = row[10][1]
+    u.email = row[6][1]
+    u.phone = row[7][1].sub('+','00')
+    u.sms_confirmation_token = row[8][1]
+    u.address = row[9][1]
     # legacy: un usuario puso una carta (literalmente)
-    if row[11][1].length < 250 
-      u.town = row[11][1]
-    else 
+    if row[10][1].length < 250
+      u.town = row[10][1]
+    else
       u.town = "A"
     end
-    u.postal_code = row[13][1]
-    u.province = PodemosImport.convert_province row[13][1], row[14][1], row[12][1]
-    u.country = PodemosImport.convert_country row[14][1]
+    u.postal_code = row[12][1]
+    u.province = PodemosImport.convert_province row[12][1], row[13][1], row[11][1]
+    u.country = PodemosImport.convert_country row[13][1]
     # legacy: al principio no se preguntaba para recibir la newsletter
-    if row[17][1] == 1
+    if row[16][1] == 1
       u.wants_newsletter = true
     end
-    u.password = row[9][1]
-    u.password_confirmation = row[9][1]
+    u.password = row[8][1]
+    u.password_confirmation = row[8][1]
     u.confirmed_at = now
     u.sms_confirmed_at = now
     u.has_legacy_password = true
-    u.created_at = row[23][1].to_datetime
-    u.circle = row[16][1] == "" ? row[16][1] : row[15][1]
+    u.created_at = row[22][1].to_datetime
+    u.circle = row[15][1] == "" ? row[14][1] : row[15][1]
     u.save
-    unless u.valid? 
+    unless u.valid?
       PodemosImport.invalid_record(u, row)
     end
   end
