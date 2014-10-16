1ac059689ca73f1349e4905de0dfbc75b19358c6
Andrés Pereira <apereira@alabs.es>
2014-10-16 11:56:23 +0200

    lopd: lock and timeouts (a little less paranoid)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -145,7 +145,7 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  config.timeout_in = 30.minutes
+  config.timeout_in = 30.days
 
   # If true, expires auth token on session timeout.
   # config.expire_auth_token_on_timeout = false
@@ -168,10 +168,10 @@ Devise.setup do |config|
 
   # Number of authentication tries before locking an account if lock_strategy
   # is failed attempts.
-  config.maximum_attempts = 5
+  config.maximum_attempts = 20
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
-  config.unlock_in = 24.hour
+  config.unlock_in = 1.hour
 
   # Warn on the last attempt before the account is locked.
   config.last_attempt_warning = true
