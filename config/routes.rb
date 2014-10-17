7750f763521ced22a7fa5ac8f23e43cc369ecdb6
Sergio Arbeo <serabe@gmail.com>
2014-10-17 23:43:18 +0200

    Error pages.

--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,10 @@ Rails.application.routes.draw do
         root 'devise/sessions#new', as: :root
       end
     end
+
+    %w(404 422 500).each do |code|
+      get code, to: 'errors#show', code: code
+    end
   end
   # /admin
   ActiveAdmin.routes(self)
