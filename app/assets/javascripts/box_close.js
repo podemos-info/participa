97d8792c9f0b1027112a77285032e37627641ba3
Sergio Arbeo <serabe@gmail.com>
2014-10-18 00:07:58 +0200

    Better flashes.

--- app/assets/javascripts/box_close.js
+++ app/assets/javascripts/box_close.js
@@ -1,6 +1,14 @@
 jQuery(function($) {
     $('.box').on('click', '.box-close', function(evt){
         evt.preventDefault();
-        $(this).parent().parent().hide("fast");
+        var box = $(this).parent().parent();
+        var divCol = box.parent();
+        var flashRoot = divCol.parent().parent();
+
+        if(divCol.children().length === 1) {
+            flashRoot.hide("fast")
+        } else {
+            box.hide("fast");
+        }
     });
 })
