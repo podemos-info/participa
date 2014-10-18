89d5a4fd32721bea6340f093bab17fa569fc0590
Andrés Pereira <apereira@alabs.es>
2014-10-18 23:47:29 +0200

    elections: actived, bug fix on timezone, bug fix on flash_boxes alert

--- app/models/election.rb
+++ app/models/election.rb
@@ -3,6 +3,6 @@ class Election < ActiveRecord::Base
   validates :title, :starts_at, :ends_at, :agora_election_id, presence: true
   has_many :votes
 
-  scope :actived, -> { where("? BETWEEN starts_at AND ends_at", Time.now.to_date)}
+  scope :actived, -> { where("? BETWEEN starts_at AND ends_at", Time.now)}
 
 end
