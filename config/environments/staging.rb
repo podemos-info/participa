29508f2ba91474853d1b1e242b9acd558d441718
Andrés Pereira <apereira@alabs.es>
2014-10-24 21:11:50 +0200

    fix: smtp instead of ses, AWS::SES::ResponseError: Throttling - Daily message quota exceeded.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -80,7 +80,19 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :ses
+  # FIXME: AWS::SES::ResponseError: Throttling - Daily message quota exceeded.
+  #config.action_mailer.delivery_method = :ses
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :address              => Rails.application.secrets.smtp["address"],
+    :user_name            => Rails.application.secrets.smtp["user_name"],
+    :password             => Rails.application.secrets.smtp["password"],
+    :domain               => Rails.application.secrets.smtp["domain"],
+    :port                 => 25,
+    :authentication       => :login,
+    :enable_starttls_auto => false
+  }
   #config.action_mailer.delivery_method = :sendmail
   # mailcatcher for testing purposes - DISABLED
   #config.action_mailer.delivery_method = :smtp
