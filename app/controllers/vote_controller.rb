7fcd103a1ebb35a8b230e08ae0ef115112ded1c7
Andrés Pereira <apereira@alabs.es>
2014-10-28 16:43:28 +0100

    #658 - Agora Voting: comprobar fechas de inicio/cierre

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -1,7 +1,15 @@
 class VoteController < ApplicationController
+
   before_action :authenticate_user! 
+  
   def create
-    vote = current_user.get_or_create_vote(params[:election_id])
-    redirect_to vote.url      
+    election = Election.find params[:election_id]
+    if election.is_actived?
+      vote = current_user.get_or_create_vote(election.id)
+      redirect_to vote.url      
+    else
+      redirect_to root_url, flash: {error: I18n.t('podemos.election.close_message') }
+    end
   end
+
 end
--- test/controllers/vote_controller_test.rb
+++ test/controllers/vote_controller_test.rb
@@ -10,12 +10,23 @@ class VoteControllerTest < ActionController::TestCase
   end
 
   test "should get create as user" do
-    e = FactoryGirl.create(:election)
+    e = FactoryGirl.create(:election, starts_at: DateTime.now-7.days, ends_at: DateTime.now+10.days)
     @user = FactoryGirl.create(:user)
     sign_in @user
     get :create, election_id: e.id
     assert_response :redirect
     assert response.header["Location"].starts_with? "https://vota.podemos.info/"
+  end
+
+  test "should give invalid date limit if election is not active" do 
+    e = FactoryGirl.create(:election, starts_at: DateTime.now-30.days, ends_at: DateTime.now-7.days)
+    @user = FactoryGirl.create(:user)
+    sign_in @user
+    get :create, election_id: e.id
+    assert_not e.is_actived?
+    assert_response :redirect
+    assert_redirected_to root_url
+    assert_equal(I18n.t('podemos.election.close_message'), flash[:error])
   end
 
 end
