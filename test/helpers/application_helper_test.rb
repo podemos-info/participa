490defda3efc459806575dde96af285c226ac394
Andrés Pereira <apereira@alabs.es>
2014-11-06 09:21:00 +0100

    helpers: remove bootstrap legacy helpers, add skiping tests for new helpers

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -5,29 +5,44 @@ class ApplicationHelperTest < ActionView::TestCase
 
   attr_reader :request 
 
-  test "should work bootstrap_class_for" do
-    assert_equal bootstrap_class_for(:success), "alert-success"
-    assert_equal bootstrap_class_for(:error), "alert-danger"
-    assert_equal bootstrap_class_for(:alert), "alert-warning"
-    assert_equal bootstrap_class_for(:notice), "alert-info"
-    assert_equal bootstrap_class_for(:another), "another"
+  test "should nav_menu_link_to work" do 
+    skip("TODO")
   end
 
-  test "should work bootstrap_nav_link" do
-    request.path = authenticated_root_path 
-    assert_equal bootstrap_nav_link("Herramientas", authenticated_root_path, "glyphicon-link"), "<li class=\"active\"><a href=\"/\"><i class=\"glyphicon glyphicon-link\"></i> Herramientas</a></li>"
-    assert_equal bootstrap_nav_link("Perfil", edit_user_registration_path, "glyphicon-cog"), "<li><a href=\"/users/edit\"><i class=\"glyphicon glyphicon-cog\"></i> Perfil</a></li>"
-
-    request.path = edit_user_registration_path 
-    assert_equal bootstrap_nav_link("Herramientas", authenticated_root_path, "glyphicon-link"), "<li><a href=\"/\"><i class=\"glyphicon glyphicon-link\"></i> Herramientas</a></li>"
-    assert_equal bootstrap_nav_link("Perfil", edit_user_registration_path, "glyphicon-cog"), "<li class=\"active\"><a href=\"/users/edit\"><i class=\"glyphicon glyphicon-cog\"></i> Perfil</a></li>"
+  test "should new_notifications_class work" do 
+    skip("TODO")
   end
 
-  test "should work bootstrap_class_for_steps" do
-    assert_equal bootstrap_class_for_steps(1,1), "active"
-    assert_equal bootstrap_class_for_steps(1,2), ""
-    assert_equal bootstrap_class_for_steps(3,2), "disabled"
-    assert_equal bootstrap_class_for_steps(2,1), "disabled"
+  test "should current_lang? work" do 
+    skip("TODO")
+  end
+
+  test "should current_lang_class work" do 
+    skip("TODO")
+  end
+
+  test "should info_box work" do 
+    skip("TODO")
+  end
+
+  test "should error_box work" do 
+    skip("TODO")
+  end
+
+  test "should field_notice_box work" do 
+    skip("TODO")
+  end
+
+  test "should errors_in_form work" do 
+    skip("TODO")
+  end
+
+  test "should steps_nav work" do 
+    skip("TODO")
+  end
+
+  test "should body_class work" do 
+    skip("TODO")
   end
 
 end
