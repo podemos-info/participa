3cf3d25f4b1518dc13d88f3f8ae54f5e978abdbf
Andrés Pereira <apereira@alabs.es>
2014-11-06 09:21:00 +0100

    helpers: remove bootstrap legacy helpers, add skiping tests for new helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,38 +1,5 @@
 module ApplicationHelper
 
-  # https://gist.github.com/roberto/3344628 
-  def bootstrap_class_for flash_type
-    case flash_type.to_sym
-    when :success
-      "alert-success"
-    when :error
-      "alert-danger"
-    when :alert
-      "alert-warning"
-    when :notice
-      "alert-info"
-    else
-      flash_type.to_s
-    end
-  end
-
-  def bootstrap_nav_link(text, path, icon)
-    options = current_page?(path) ? { class: "active" } : {}
-    content_tag(:li, options) do
-      link_to content_tag(:i, "", class: "glyphicon #{icon}") + " " + text, path
-    end
-  end
-
-  def bootstrap_class_for_steps(step_n, step_current)
-    if step_current == step_n
-      "active"
-    elsif step_current > step_n
-      ""
-    else
-      "disabled"
-    end
-  end
-
   # Like link_to but third parameter is an array of options for current_page?.
   def nav_menu_link_to name, url, current_urls, html_options = {}
     html_options[:class] ||= ""
