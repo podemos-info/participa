c1fb9667bbb0f4e3045ff4b7e592795c694664c7
Andrés Pereira <apereira@alabs.es>
2014-11-24 15:50:04 +0100

    collaborations: orders model

---
+++ test/factories/orders.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :order do
+    collaboration_id 1
+    status 1
+    payable_at "2014-11-23 00:24:45"
+    payed_at "2014-11-23 00:24:45"
+    deleted_at "2014-11-23 00:24:45"
+  end
+end
