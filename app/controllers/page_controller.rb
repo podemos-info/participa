1883d0772150a87f2a25de8f545d963be986cf9d
Andrés Pereira <apereira@alabs.es>
2014-12-04 13:17:59 +0100

    pages: Comisión de Garantías (guarantees) for logged in users

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,9 +1,14 @@
 class PageController < ApplicationController
 
+  before_action :authenticate_user!, only: :guarantees
+
   def privacy_policy
   end
 
   def faq
+  end
+
+  def guarantees
   end
 
 end
