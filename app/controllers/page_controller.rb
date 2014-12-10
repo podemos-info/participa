31075ff57b1791301b181629314249e21390cb07
Andrés Pereira <apereira@alabs.es>
2014-12-10 18:08:57 +0100

    pages: guarantees - intro

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -6,13 +6,16 @@ class PageController < ApplicationController
   def faq
   end
 
+  def guarantees
+  end
+
   def guarantees_conflict
   end
 
   def guarantees_compliance
   end
 
-  def guarantees_ethics
+  def guarantees_ethic
   end
 
 end
