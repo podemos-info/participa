58fcd9af0178c79621b94f2c7d9367dfa356e4a2
Andrés Pereira <apereira@alabs.es>
2014-12-12 10:18:20 +0100

    tests: fix failing tests (changes on controllers)

--- test/controllers/page_controller_test.rb
+++ test/controllers/page_controller_test.rb
@@ -17,7 +17,7 @@ class PageControllerTest < ActionController::TestCase
     assert_response :success
     get :guarantees_compliance
     assert_response :success
-    get :guarantees_ethics
+    get :guarantees_ethic
     assert_response :success
   end
 
@@ -28,7 +28,7 @@ class PageControllerTest < ActionController::TestCase
     assert_response :success
     get :guarantees_compliance
     assert_response :success
-    get :guarantees_ethics
+    get :guarantees_ethic
     assert_response :success
   end
 
