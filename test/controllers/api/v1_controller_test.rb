26a387e7a96f299793c3b26839dacbf5a155b9b3
Andrés Pereira <apereira@alabs.es>
2014-12-12 10:18:20 +0100

    tests: fix failing tests (changes on controllers)

--- test/controllers/api/v1_controller_test.rb
+++ test/controllers/api/v1_controller_test.rb
@@ -37,13 +37,13 @@ class Api::V1ControllerTest < ActionController::TestCase
 
   test "should post to registrate" do
     id = "1111111"
-    post :gcm_registrate, notice_registrar: {registration_id: id}
+    post :gcm_registrate, v1: {registration_id: id}
     assert_equal id, NoticeRegistrar.find_by_registration_id(id).registration_id
     assert_equal 1, NoticeRegistrar.all.count
-    post :gcm_registrate, notice_registrar: {registration_id: "2222222"}
+    post :gcm_registrate, v1: {registration_id: "2222222"}
     assert_equal 2, NoticeRegistrar.all.count
     assert_response 201
-    post :gcm_registrate, notice_registrar: {registration_id: "2222222"}
+    post :gcm_registrate, v1: {registration_id: "2222222"}
     assert_equal 2, NoticeRegistrar.all.count
     assert_response 201
   end
