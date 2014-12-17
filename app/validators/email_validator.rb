1fb3838235dd50d6f7f7b81c5a658dc9534884c2
Podemos <informatica@podemos.info>
2014-12-17 22:38:40 +0100

    Bugs fixed and tests created for email validation.

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -3,39 +3,40 @@ require 'mail'
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record,attribute,value)
 
-    # First check, with the whole string
-    if (value.downcase =~ /[àèìòùáéíóúñç]/) != nil
-      record.errors[attribute] << "La dirección de correo no puede contener acentos, eñes u otros caracteres especiales"
-      return false
-    elsif value.include? ".." 
-      record.errors[attribute] << "La dirección de correo no puede contener dos puntos seguidos"
-      return false
-    elsif (value =~ /^[a-zA-Z0-9]/) == nil
-      record.errors[attribute] << "La dirección de correo debe comenzar con un número o una letra"
-      return false
-    elsif (value =~ /[a-zA-Z]$/) == nil
-      record.errors[attribute] << "La dirección de correo debe acabar con una letra"
-      return false
+    if value.length==0
+      return true
     end
 
-    begin
-      m = Mail::Address.new(value)
+    error = false
 
-      # domain validation
-      if m.domain.include? "," or not m.domain.include? "."
-        record.errors[attribute] << "La dirección de correo contiene caracteres inválidos"
-        return false
+    # First check, with the whole string
+    if (value.downcase =~ /[ÁÉÍÓÚàèìòùÀÈÍÓÚáéíóúñÑçÇ]/) != nil
+      error = "La dirección de correo no puede contener acentos, eñes u otros caracteres especiales"
+    elsif value.include? ".." 
+      error = "La dirección de correo no puede contener dos puntos seguidos"
+    elsif (value =~ /^["a-zA-Z0-9]/) == nil
+      error = "La dirección de correo debe comenzar con un número o una letra"
+    elsif (value =~ /[a-zA-Z]$/) == nil
+      error = "La dirección de correo debe acabar con una letra"
+    else
+      begin
+        m = Mail::Address.new(value)
+
+        # when an unquoted comma is found, the parsed address is different than the received string
+        if value.include? "," and m.address != value
+          error = "La dirección de correo contiene caracteres inválidos"
+        elsif not m.domain.include? "." # domain validation
+          error = "La dirección de correo es incorrecta"
+        end
+      rescue
+        error = "La dirección de correo es incorrecta"
       end
+    end
 
-      # name validation
-      if m.name.include? "," and (not m.name.start_with '"' or not m.name.end_with '"')
-        record.errors[attribute] << "La dirección de correo contiene caracteres inválidos"
-        return false
-      end
-
-    rescue Exception => e   
-      record.errors[attribute] << "La dirección de correo no es correcta"
-      r = false
+    if error
+      record.errors.delete(attribute)
+      record.errors[attribute] << error
+      return false
     end
   end
 end
