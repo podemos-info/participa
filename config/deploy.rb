58ed9b9e7ee7f12b9f1542bf6e78948d999159f6
Andrés Pereira <apereira@alabs.es>
2014-12-17 12:59:33 +0100

    deploy: rename to podemos participa

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,6 @@
 lock '3.2.1'
 
 set :application, 'juntos.podemos.info'
-set :repo_url, 'git@github.com:podemos-info/podemos-juntos.git'
+set :repo_url, 'git@github.com:podemos-info/podemos-participa.git'
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
