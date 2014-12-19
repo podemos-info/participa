8081d8e424cf24beffb081d23245b7b0a58ca382
Andrés Pereira <apereira@alabs.es>
2014-12-19 17:48:34 +0100

    js: removing intlTelInput for phone international (deprecated)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.sidr.min
-//= require intlTelInput
 //= require select2
 //= require cookie_policy
 //= require registrations_submenu
