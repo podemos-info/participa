61c105e3abb2f45ac7478228d80f94b7fdde9598
Andrés Pereira <apereira@alabs.es>
2014-12-26 18:55:30 +0100

    deploy: listen TCP socket on unicorn

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -2,6 +2,6 @@ working_directory "/var/www/participa.podemos.info/current"
 pid "/var/www/participa.podemos.info/current/tmp/pids/unicorn.pid"
 stderr_path "/var/www/participa.podemos.info/shared/log/unicorn.log"
 stdout_path "/var/www/participa.podemos.info/shared/log/unicorn.log"
-listen "/tmp/unicorn.participa.podemos.info.sock"
+listen "127.0.0.1:8080"
 worker_processes 24
 timeout 120
