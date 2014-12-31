0b909e32adac81ce8d6bfa6c4f2206157a77cecf
Podemos <informatica@podemos.info>
2014-12-31 19:50:54 +0100

    Added export_data_slow.

--- lib/podemos_export.rb
+++ lib/podemos_export.rb
@@ -10,3 +10,14 @@ def export_data(filename, query)
     end
   end
 end
+
+def export_data_slow(filename, query)
+  # Usage: export_data {filename}, {query (ej: User.select(:id).where(born_at: nil))} do |row| [ { row.field, row.field, ... } ] end
+  # Output will be located at tmp/export folder.
+  FileUtils.mkdir_p("tmp/export") unless File.directory?("tmp/export")
+  CSV.open( "tmp/export/#{filename}.tsv", 'w', { :col_sep => "\t"} ) do |writer|
+    query.each do |item|
+      writer << yield(item)
+    end
+  end
+end
