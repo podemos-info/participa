148dbffc8195aed5be55e9b01a996e04420ad265
Podemos <informatica@podemos.info>
2015-01-16 16:07:45 +0000

    Fixes.

--- test/controllers/participation_teams_controller_test.rb
+++ test/controllers/participation_teams_controller_test.rb
@@ -1,14 +1,23 @@
 require 'test_helper'
 
 class ParticipationTeamsControllerTest < ActionController::TestCase
-  setup do
-    @participation_team = participation_teams(:one)
+
+  test "should authenticate user" do
+    get :new
+    assert_response :redirect
+    assert_redirected_to "/users/sign_in" # FIXME bug con locales
   end
 
-  test "should get index" do
+  test "should get index without want_participation" do
+    user = FactoryGirl.create(:user)
+    sign_in user
     get :index
     assert_response :success
     assert_not_nil assigns(:participation_teams)
+  end
+
+  test "should get index with want_participation" do
+    skip
   end
 
   test "should join a team" do
