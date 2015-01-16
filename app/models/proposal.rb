920f56e6c3191f817eea0a2bc95d7226b23f5b30
rgarcia <voodoorai2000@gmail.com>
2015-01-16 14:49:45 +0100

    Basic proposal show #29

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -8,15 +8,22 @@ class Proposal
     url_base = 'http://api.reddit.com/r/Podemos/search?q=flair%3APropuestas&sort='
     url_base + type + '&restrict_sr=on&t=all&limit=' + '10'    
   end
-
+    
   def self.reddit_proposals    
     parse(JSON.load(open(reddit_url))["data"]["children"])
+  end
+
+  def self.reddit_proposal(id)
+    url = "http://www.reddit.com/by_id/#{id}.json"
+    parse(JSON.load(open(url))["data"]["children"]).first
   end
 
   def self.parse(json)
     json.collect {|proposal| 
       Proposal.new(title:       proposal["data"]["title"],
-                   description: proposal["data"]["selftext"],)
+                   description: proposal["data"]["selftext"],
+                   reddit_url:  proposal["data"]["url"],
+                   reddit_id:   proposal["data"]["name"])
     }
   end
 end
