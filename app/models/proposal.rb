318f7e5fdf6eed620db45dc2a1934909be7a4bd5
rgarcia <voodoorai2000@gmail.com>
2015-01-16 15:34:43 +0100

    Refactoring proposals

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -3,23 +3,28 @@ class Proposal
   include ActiveModel::Model
   attr_accessor :title, :description, :reddit_url, :reddit_id
     
-  def self.reddit_proposals(filter='hot')
-    url_base = 'http://api.reddit.com/r/Podemos/search?q=flair%3APropuestas&sort='
-    url = url_base + filter + '&restrict_sr=on&t=all&limit=' + '10'     
-    parse(JSON.load(open(url))["data"]["children"])
+  def self.base_url
+    "http://api.reddit.com"
   end
-
+    
   def self.reddit_proposal(id)
-    url = "http://www.reddit.com/by_id/#{id}.json"
-    parse(JSON.load(open(url))["data"]["children"]).first
+    url = "#{base_url}/by_id/#{id}"
+    parse(url).first
   end
 
-  def self.parse(json)
+  def self.reddit_proposals(filter='hot')
+    url = "#{base_url}/r/Podemos/search?q=flair%3APropuestas&sort=#{filter}&restrict_sr=on&t=all&limit=10"
+    parse(url)
+  end
+
+  def self.parse(url)
+    json = JSON.load(open(url))["data"]["children"]
     json.collect {|proposal| 
-      Proposal.new(title:       proposal["data"]["title"],
-                   description: proposal["data"]["selftext"],
-                   reddit_url:  proposal["data"]["url"],
-                   reddit_id:   proposal["data"]["name"])
+      data = proposal["data"]
+      Proposal.new(title:       data["title"],
+                   description: data["selftext"],
+                   reddit_url:  data["url"],
+                   reddit_id:   data["name"])
     }
   end
 end
