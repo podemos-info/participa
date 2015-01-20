97b40bdeff56b4d5b76a6fa43f53af818cf4395f
Podemos <informatica@podemos.info>
2015-01-20 00:06:01 +0000

    Fixed problem changing password with a profile with errors.

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -8,16 +8,15 @@ class PasswordsController < Devise::PasswordsController
   def update
     self.resource = resource_class.reset_password_by_token(resource_params)
     yield resource if block_given?
-    if resource.errors.empty?
+    if not resource.errors.include? :password and not resource.errors.include? :password_confirmation
       resource.update_attribute(:has_legacy_password, false) if resource.has_legacy_password?
       resource.unlock_access! if unlockable?(resource)
       flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
       set_flash_message(:notice, flash_message) if is_flashing_format?
       sign_in(resource_name, resource)
-      respond_with resource, location: after_resetting_password_path_for(resource)
+      redirect_to after_resetting_password_path_for(resource)
     else
       respond_with resource
     end
   end
-
 end
