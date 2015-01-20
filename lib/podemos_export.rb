24f536de3e84798f1f42872a478a7c40cb60812b
Podemos <informatica@podemos.info>
2015-01-20 16:50:55 +0000

    Daily census uses different folder for its output.

--- lib/podemos_export.rb
+++ lib/podemos_export.rb
@@ -3,8 +3,8 @@ require 'csv'
 def export_data(filename, query, headers = nil, folder = "tmp/export")
   # Usage: export_data {filename}, {query (ej: User.select(:id).where(born_at: nil))} do |row| [ { row.field, row.field, ... } ] end
   # Output will be located at tmp/export folder.
-  FileUtils.mkdir_p("tmp/export") unless File.directory?(folder)
-  CSV.open( "tmp/export/#{filename}.tsv", 'w', { :col_sep => "\t"} ) do |writer|
+  FileUtils.mkdir_p(folder) unless File.directory?(folder)
+  CSV.open( "#{folder}/#{filename}.tsv", 'w', { :col_sep => "\t"} ) do |writer|
     writer << headers if headers
     query.find_each do |item|
       res = yield(item)
@@ -17,8 +17,8 @@ end
 
 def export_raw_data(filename, data, headers = nil, folder = "tmp/export")
   # Output will be located at tmp/export folder.
-  FileUtils.mkdir_p("tmp/export") unless File.directory?(folder)
-  CSV.open( "tmp/export/#{filename}.tsv", 'w', { :col_sep => "\t"} ) do |writer|
+  FileUtils.mkdir_p(folder) unless File.directory?(folder)
+  CSV.open( "#{folder}/#{filename}.tsv", 'w', { :col_sep => "\t"} ) do |writer|
     writer << headers if headers
     data.each do |item|
       res = yield(item)
