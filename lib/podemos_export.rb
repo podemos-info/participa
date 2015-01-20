2c914d3f780b14d9faafae89a7d150e7f67ced83
Podemos <informatica@podemos.info>
2015-01-20 16:22:17 +0000

    Improvements in export data task.

--- lib/podemos_export.rb
+++ lib/podemos_export.rb
@@ -1,10 +1,11 @@
 require 'csv'
 
-def export_data(filename, query)
+def export_data(filename, query, headers = nil)
   # Usage: export_data {filename}, {query (ej: User.select(:id).where(born_at: nil))} do |row| [ { row.field, row.field, ... } ] end
   # Output will be located at tmp/export folder.
   FileUtils.mkdir_p("tmp/export") unless File.directory?("tmp/export")
   CSV.open( "tmp/export/#{filename}.tsv", 'w', { :col_sep => "\t"} ) do |writer|
+    writer << headers if headers
     query.find_each do |item|
       res = yield(item)
       if res
@@ -14,12 +15,12 @@ def export_data(filename, query)
   end
 end
 
-def export_data_slow(filename, query)
-  # Usage: export_data {filename}, {query (ej: User.select(:id).where(born_at: nil))} do |row| [ { row.field, row.field, ... } ] end
+def export_raw_data(filename, data, headers = nil)
   # Output will be located at tmp/export folder.
   FileUtils.mkdir_p("tmp/export") unless File.directory?("tmp/export")
   CSV.open( "tmp/export/#{filename}.tsv", 'w', { :col_sep => "\t"} ) do |writer|
-    query.each do |item|
+    writer << headers if headers
+    data.each do |item|
       res = yield(item)
       if res
         writer << res
