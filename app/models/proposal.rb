eac8e1055e06e49fa53c80a2c58f88ffa936f3b3
rgarcia <voodoorai2000@gmail.com>
2015-01-22 13:18:39 +0100

    Extract Reddit API parsing to rake task. Closes #38

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,35 +1,5 @@
-require 'open-uri'
-class Proposal
-  include ActiveModel::Model
-  attr_accessor :title, :description, :votes, :reddit_url, :reddit_id
+class Proposal < ActiveRecord::Base
     
-  def self.base_url
-    "http://api.reddit.com"
-  end
-    
-  def self.reddit_proposal(id)
-    url = "#{base_url}/by_id/#{id}"
-    parse(url).first
-  end
-
-  def self.reddit_proposals(filter='hot')
-    url = "#{base_url}/r/Podemos/search?q=flair%3APropuestas&sort=#{filter}&restrict_sr=on&t=all&limit=10"
-    parse(url)
-  end
-
-  def self.parse(url)
-    json = JSON.load(open(url))["data"]["children"]
-    json.collect {|proposal| 
-      data = proposal["data"]
-      proposal = Proposal.new(title:       data["title"],
-                   description: data["selftext"],
-                   votes:       data["ups"],
-                   reddit_url:  data["url"],
-                   reddit_id:   data["name"]) 
-      proposal if proposal.reddit_required_votes?
-    }.compact
-  end
-
   def approval
     votes.percent_of(confirmed_users)
   end
