1eaf406c30ec8474dc42834649c4b8a11371ec8d
Podemos <informatica@podemos.info>
2015-02-08 15:04:54 +0000

    Stores credits database on shared location to avoid errors after deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,5 +3,5 @@ lock '3.2.1'
 
 set :application, 'juntos.podemos.info'
 set :repo_url, 'git@github.com:podemos-info/podemos-participa.git'
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml db/podemos/credits.tsv}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
