e3caddbc63a101f1e7f8844f31c0bb80d39cf6a9
Podemos <informatica@podemos.info>
2015-02-20 19:25:12 +0000

    Open form.

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -5,7 +5,7 @@ class PageController < ApplicationController
                                               :guarantees_ethic, :circles_validation, :primarias_andalucia, :listas_primarias_andaluzas,
                                               :responsables_organizacion_municipales, :credits, :credits_add, :credits_info,
                                               :responsables_municipales_andalucia, :plaza_podemos_municipal,
-                                              :portal_transparencia_cc_estatal, :mujer_igualdad]
+                                              :portal_transparencia_cc_estatal, :mujer_igualdad, :alta_consulta_ciudadana]
 
   def privacy_policy
   end
