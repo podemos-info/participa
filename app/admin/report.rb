ddfaa8f1136081526faab5653fbdbcd403c1ca04
Podemos <informatica@podemos.info>
2015-03-03 11:28:32 +0000

    Fixes on reports

--- app/admin/report.rb
+++ app/admin/report.rb
@@ -3,6 +3,7 @@ ActiveAdmin.register Report do
 
   permit_params  :title, :query, :main_group, :groups
 
+
   index do
     selectable_column
     id_column
@@ -13,5 +14,30 @@ ActiveAdmin.register Report do
     end
     actions
   end
+
+  show do
+    if resource.results
+      @groups = YAML.load(resource.groups)
+      @results = YAML.load(resource.results)
+      @groups.each do |group|
+        panel group.title do
+          data = @results[:data][group.id].select {|x| !group.whitelist? x[:name]}
+          table_for data do
+            column group.label, :name
+            column "Total" do |r|
+              r[:count]
+            end
+            column group.data_label do |r|
+              r[:samples].join(", ")  if r[:samples]
+            end
+            column :users do |r|
+              r[:users][0..20].map do |u| link_to(u, admin_user_path(u)).html_safe end .join(" ").html_safe if r[:users]
+            end
+          end
+        end
+      end
+    end
+  end
+
 
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -24,66 +24,73 @@ class Report < ActiveRecord::Base
 
     # Initialize
     tmp_results = { data: {}, errors: { fetch: [] } }
+
     folder = "#{Rails.root}/tmp/report/#{id}"
     raw_folder = "#{folder}/raw"
     rank_folder = "#{folder}/rank"
-    FileUtils.mkdir_p(raw_folder) unless File.directory?(raw_folder)
-    FileUtils.mkdir_p(rank_folder) unless File.directory?(rank_folder)
-
-    @groups.each { |group| group.create_temp_file raw_folder }
     
     # Aggregation data
     id_width = @relation.maximum(:id).to_s.length
     total = @relation.count
 
-    # Browse data
-    main_name = ""
-    @relation.find_each do |row|
-      row_id = row.id.to_s.ljust(id_width)
+    if not File.directory?(raw_folder)
+      FileUtils.mkdir_p(raw_folder) unless File.directory?(raw_folder)
+      FileUtils.mkdir_p(rank_folder) unless File.directory?(rank_folder)
 
-      main_name = @main_group.format_group_name(@main_group.process(row)[0]) if @main_group
+      @groups.each { |group| group.create_temp_file raw_folder }
+      # Browse data
+      main_name = ""
+      @relation.find_each do |row|
+        row_id = row.id.to_s.ljust(id_width)
 
-      @groups.each do |group|
-        width = group.width
-        begin
-          group.process(row).each do |name, data|
-            group.write "#{row_id}#{main_name}#{group.format_group_name(name)}#{data}"
+        main_name = @main_group.format_group_name(@main_group.process(row)[0]) if @main_group
+
+        @groups.each do |group|
+          width = group.width
+          begin
+            group.process(row).each do |name, data|
+              group.write "#{row_id} #{main_name}#{group.format_group_name(name)} #{data}"
+            end
+          rescue Exception => e
+            tmp_results[:errors][:fetch] = [ e.message, e.backtrace.inspect ]
           end
-        rescue Exception => e
-          tmp_results[:errors][:fetch] = [ e.message, e.backtrace.inspect ]
         end
       end
+      @groups.each { |group| group.close_temp_file }
     end
-    @groups.each { |group| group.close_temp_file }
 
     # Generate rank
     main_width = @main_group ? @main_group.width : 0
     @groups.each do |group|
       rest = total
       width = group.width
-      %x(sort -k2,2 #{raw_folder}/#{group.id}.dat | uniq -s#{id_width} -w#{width+main_width} -c | sort -rn > #{rank_folder}/#{group.id}.dat)
+      %x(sort -k2,2 #{raw_folder}/#{group.id}.dat | uniq -s#{id_width+1} -w#{width+main_width} -c | sort -rn > #{rank_folder}/#{group.id}.dat)
 
       tmp_results[:data][group.id] = @main_group ? Hash.new { |h,k| h[k] = [] } : []
 
       File.open( "#{rank_folder}/#{group.id}.dat", 'r:UTF-8' ).each do |line|
-        data = line.split("\s", 2)
+        data = line.split("\s", 3)
         count = data[0].to_i
 
         if count <= group.minimum
-          main_name = nil
           result = { count: rest, name: group.minimum_label }
+          tmp_results[:data][group.id] << result
+          break
         else
           rest -= count
 
-          main_name = @main_group.format_group_name(data[1][id_width..(id_width+main_width-1)]) if @main_group
-          name = data[1][id_width+main_width..(id_width+main_width+width-1)]
+          main_name = @main_group.format_group_name(data[2][0..(main_width-1)]) if @main_group
+          name = data[2][main_width..(main_width+width-1)]
 
           result = { count: count, name: name.strip, users:[], samples:[]}
-
-          %x(grep  "#{'.'*id_width}#{main_name}#{group.format_group_name(name)}" #{raw_folder}/#{group.id}.dat | head -n1000).split("\n").each do |s|
+          p name.strip
+          %x(grep  "#{'.'*id_width} #{main_name}#{group.format_group_name(name)} " #{raw_folder}/#{group.id}.dat | head -n#{[count,100].min}).split("\n").each do |s|
             result[:users] << s[0..id_width].to_i
-            result[:samples] << s[(id_width+main_width+width)..-1]
+            sample = s[(id_width+1+main_width+width+1)..-1].strip
+            result[:samples] << sample if not sample.empty?
           end
+
+          result[:samples].uniq!
         end
 
         if @main_group
