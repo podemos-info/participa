cedc14444b436127c8646bd1b062116832f7568b
Podemos <informatica@podemos.info>
2015-03-09 10:08:19 +0000

    Allows to enter to non admin users to collaborations.

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -1,7 +1,7 @@
 class CollaborationsController < ApplicationController
   
   before_action do |controller|
-    raise ActionController::RoutingError.new('Not Found') if Rails.env.production? and not current_user.admin
+    raise ActionController::RoutingError.new('Not Found') if Rails.env.production?
   end
 
   before_action :authenticate_user!
