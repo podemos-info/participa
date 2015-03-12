78b43601ee958d7dd484835059f904937c26dbcb
rgarcia <voodoorai2000@gmail.com>
2015-03-12 18:33:20 +0100

    Adding custom image to proposals

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312164834) do
+ActiveRecord::Schema.define(version: 20150312172757) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -134,6 +134,7 @@ ActiveRecord::Schema.define(version: 20150312164834) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "reddit_threshold", default: false
+    t.string   "image_url"
   end
 
   create_table "report_groups", force: true do |t|
