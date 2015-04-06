968262326b3980a09f308cc7a66dda4c3ac0559d
Podemos <informatica@podemos.info>
2015-04-06 23:40:56 +0000

    Fixed PDF generation.

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -4,7 +4,7 @@ class UsersMailer < ActionMailer::Base
   def microcredit_email(microcredit, loan)
     @microcredit = microcredit
     @loan = loan
-    attachments['IngresoMicrocreditosPodemos.pdf'] = render_to_string pdf: 'IngresoMicrocreditosPodemos.pdf', template: 'microcredit/email_guide.pdf.erb', encoding: "UTF-8"
+    attachments['IngresoMicrocreditosPodemos.pdf'] = WickedPdf.new.pdf_from_string(render_to_string pdf: 'IngresoMicrocreditosPodemos.pdf', template: 'microcredit/email_guide.pdf.erb', encoding: "UTF-8")
     mail(from: '"Podemos" <microcreditos@podemos.info>', to: @loan.email, subject: 'Confirmación microcréditos Podemos')
   end
 end
