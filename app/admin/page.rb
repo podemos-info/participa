ec33c15ab39131b8926e90ef2ac87d9e657dd270
Iván Eixarch <ivan@sinanimodelucro.org>
2015-04-16 15:58:25 +0200

    add button for refresh routes in all instances of unicorn from admin panel

--- app/admin/page.rb
+++ app/admin/page.rb
@@ -2,6 +2,15 @@ ActiveAdmin.register Page do
 
   permit_params :id_form, :title, :slug, :require_login
 
+  action_item do
+    link_to('Recargar rutas', reload_admin_pages_path, data: { confirm: "¿Estas segura de querer recargar las rutas?" })
+  end
+
+  collection_action :reload, :method => :get do
+    Process.kill('HUP', Process.ppid()) if !Rails.env.development?
+    redirect_to collection_path, alert: "Las rutas han sido recargadas."
+  end
+
   form do |f|
     f.semantic_errors
     f.inputs 'Formulario de gravity' do
