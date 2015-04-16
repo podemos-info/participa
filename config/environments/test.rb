dbaa9a9843395b1d2fa04be7acf3c43b8e389508
Podemos <informatica@podemos.info>
2015-04-16 20:47:55 +0000

    Disable paper_trail during tests.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  # Disable paper_trail during tests
+  PaperTrail.enabled = false
 end
