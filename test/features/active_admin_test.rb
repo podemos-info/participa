d71dd750bf07b794e749aa25a2704e805187ba1f
Andrés Pereira <apereira@alabs.es>
2015-07-03 15:52:34 +0200

    [tests] [admin] check that works (view index on users)

---
+++ test/features/active_admin_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+feature "Active Admin Test" do
+
+  scenario "normal user shouldn't access the admin" do
+    visit admin_users_path
+    page.must_have_content "No tienes permisos para acceder a esa sección "
+
+    user = FactoryGirl.create(:user)
+    login_as(user)
+    visit admin_collaborations_path
+    page.must_have_content "No tienes permisos para acceder a esa sección "
+  end
+
+  scenario "admin user should access the admin" do
+    user = FactoryGirl.create(:user, :admin)
+    login_as(user)
+    visit admin_users_path
+    save_and_open_page
+    page.must_have_content "Salir"
+    page.must_have_content "Usuario"
+    page.must_have_content "Perez Pepito"
+  end
+
+end 
