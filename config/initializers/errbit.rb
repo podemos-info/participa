9024307c57e6ddcb9f2a510d93c364cca88e803a
Andrés Pereira <apereira@alabs.es>
2015-07-04 13:00:51 +0200

    [secrets] missing airbrake.port key

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,7 @@
 Airbrake.configure do |config|
   config.api_key = Rails.application.secrets.airbrake["api_key"]
   config.host    = Rails.application.secrets.airbrake["host"]
-  config.port    = 80
+  config.port    = Rails.application.secrets.airbrake["port"]
   config.secure  = config.port == 443
 end
 
