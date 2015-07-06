0f3d7de15950b4ffac95a9de04354d4968828e21
Andrés Pereira <apereira@alabs.es>
2015-07-06 13:05:31 +0200

    [deploy] unicorn workers

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -3,5 +3,5 @@ pid "/var/www/participa.podemos.info/current/tmp/pids/unicorn.pid"
 stderr_path "/var/www/participa.podemos.info/shared/log/unicorn.log"
 stdout_path "/var/www/participa.podemos.info/shared/log/unicorn.log"
 listen "127.0.0.1:8080"
-worker_processes 24
+worker_processes 16
 timeout 240
