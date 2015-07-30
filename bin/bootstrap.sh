2d9874ec2667dda5e5f458dd041c66b48e8a89ae
David A. Lareo <dalareo@gmail.com>
2015-07-30 10:42:56 +0200

    Add gpg keys

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -19,7 +19,8 @@ cat > /home/vagrant/deploy.sh <<EOF
 
 set -x
 
-if [ ! -d ~/.rvm ] ; then 
+if [ ! -d ~/.rvm ] ; then
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   curl -sSL https://get.rvm.io | bash -s stable 
 fi
 
