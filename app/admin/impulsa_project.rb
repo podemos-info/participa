622a5f62ce85e67e99e2ebb7496274d900f88fe6
Iván Eixarch <ivan@sinanimodelucro.org>
2015-10-10 23:47:02 +0200

    Add logo image to impulsa project index admin

--- app/admin/impulsa_project.rb
+++ app/admin/impulsa_project.rb
@@ -9,21 +9,24 @@ ActiveAdmin.register ImpulsaProject do
   filter :impulsa_edition_category, as: :select, collection: -> { parent.impulsa_edition_categories}
   filter :name
   filter :user_id
+  filter :id_in, as: :text, label: "Lista de IDs de proyectos", required: false
   filter :user_email_contains
   filter :authority
   filter :authority_name
-  filter :id_in, as: :text, label: "Lista de IDs de proyectos", required: false
 
   index download_links: -> { can?(:admin, ImpulsaProject) } do
     selectable_column
     column :id
+    column :logo do |impulsa_project|
+      image_tag(impulsa_project.logo.url(:thumb))
+    end
     column :name do |impulsa_project|
       link_to impulsa_project.name, admin_impulsa_edition_impulsa_project_path(impulsa_edition, impulsa_project)
     end
     column :user
     column :total_budget
     column :impulsa_edition_category
-    column :updated_at
+    #column :updated_at
     column :status_name do |impulsa_project|
       div t("podemos.impulsa.project_status.#{ImpulsaProject::PROJECT_STATUS.invert[impulsa_project.status]}")
       if impulsa_project.saveable?
