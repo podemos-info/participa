475e640824aacd230720975a6b0186b50090f354
Leonardo Diez <leiodd@gmail.com>
2015-11-18 00:34:15 +0100

    [microcredits] Improvements on auto confirmations.

--- app/admin/microcredit.rb
+++ app/admin/microcredit.rb
@@ -205,13 +205,14 @@ ActiveAdmin.register Microcredit do
       muser = movement[:concept][0..37].strip.downcase
       mconcept = movement[:concept][38..-1]
       id, mname = mconcept.split("-") if mconcept
+
       if mconcept && mname && mname.downcase==resource.title.downcase && id.to_i>0
         sure = true
         temp = resource.loans.where(id: id.to_i)
       end
 
-      if sure and temp.length==1 && temp.amount == movement[:amount] && "#{temp.last_name} #{temp.first_name}".downcase[0..37].strip==muser
-        loans[:sure] << { loan: temp, movement: movement }
+      if sure and temp.length==1 && temp.first.amount == movement[:amount] && ("#{temp.first.last_name} #{temp.first.first_name}".downcase[0..37].strip==muser || "#{temp.first.first_name} #{temp.first.last_name}".downcase[0..37].strip==muser)
+        loans[:sure] << { loans: temp, movement: movement }
         next
       end
 
