0185e69f04b3e47f3d17e5f9b1263b9d5fcd180f
Andrés Pereira <apereira@alabs.org>
2016-05-10 23:06:25 +0200

    [tests] proposals disable on tests

--- test/features/proposals_test.rb
+++ test/features/proposals_test.rb
@@ -1,24 +1,29 @@
 require "test_helper"
 
-feature "Proposals" do
-  
-  scenario "Support", js: true do
-    user = FactoryGirl.create(:user)
-    proposal = FactoryGirl.create(:proposal)
+if Rails.application.secrets.features["proposals"]
 
-    login_as(user)
-
-    visit proposals_path
-    page.must_have_content "Iniciativas Ciudadanas"
+  feature "Proposals" do
     
-    click_button "Apoyar propuesta"
-    page.must_have_content "¡Muchas gracias!"
-
-    visit proposal_path(id: proposal)
-    save_and_open_page
-    page.must_have_content "Ya has apoyado esta propuesta. ¡Muchas gracias!"
-
-    # TODO Proposal.frozen?
+    scenario "Support", js: true do
+      user = FactoryGirl.create(:user)
+      proposal = FactoryGirl.create(:proposal)
+  
+      login_as(user)
+  
+      visit proposals_path
+  
+      page.must_have_content "Iniciativas Ciudadanas"
+      
+      click_button "Apoyar propuesta"
+      page.must_have_content "¡Muchas gracias!"
+  
+      debugger
+      visit proposal_path(id: proposal)
+      page.must_have_content "Ya has apoyado esta propuesta. ¡Muchas gracias!"
+  
+      # TODO Proposal.frozen?
+    end
+  
   end
 
 end
