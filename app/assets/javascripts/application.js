4fea2a5063851bc56d7d638361c4c95d2fd67182
Andrés Pereira <apereira@alabs.org>
2016-05-11 17:06:05 +0200

    [verifications] leaflet missing assets and favicon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery.sidr.min
 //= require iframeSizer.min
+//= require leaflet
 //= require select2
 //= require cookie_policy
 //= require registrations_submenu
