2a338058f12ed9d350363231ad8d904d9dfc43e3
Leonardo Diez <leiodd@gmail.com>
2016-11-21 18:40:47 +0100

    [impulsa] Fixed impulsa project admin page.

--- app/admin/impulsa_project.rb
+++ app/admin/impulsa_project.rb
@@ -340,7 +340,7 @@ ActiveAdmin.register ImpulsaProject do
       resource = active_admin_config
 
       ImpulsaProject.state_machine.states.keys.each do |state|
-        if resource.scopes.none? { |scope| scope.name.to_s == state.to_s }
+        if resource.scopes.none? { |scope| scope.id.to_s == state.to_s }
           resource.scopes << ActiveAdmin::Scope.new( state ) do |projects| projects.where(state: state) end
         end
       end
