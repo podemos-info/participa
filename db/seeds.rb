58ca96b6766b2cec514fe604138da401e0bae840
Andrés Pereira <apereira@alabs.org>
2016-11-23 10:20:05 +0100

    [seeds] Adding users seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'database_cleaner'
+
+DatabaseCleaner.clean_with :truncation
+pw = '123456789'
+
+puts "Creating Users"
+
+admin = FactoryGirl.create(:user, :admin, password: pw, email: "admin@example.com") 
+admin.verifications_admin = true
+admin.save
+puts "Creating admin user with email #{admin.email}"
+
+superadmin = FactoryGirl.create(:user, :superadmin, password: pw, email: "superadmin@example.com") 
+puts "Creating superadmin user with email #{superadmin.email}"
+
+(0..10).each do |i| 
+  user = FactoryGirl.create(:user, password: pw) 
+  puts "Creating unverified user with email #{user.email}"
+end
+
+(0..10).each do |i| 
+  user = FactoryGirl.create(:user, password: pw) 
+  user.verify! admin
+  puts "Creating verified user with email #{user.email}"
+end
