5e31aecfdd61a23841ef1876bf68ab4bfc738701
Enrique Alvarez <enrique@codecantor.com>
2016-12-02 15:54:06 +0200

    Remove unused files and add examples

--- Capfile
+++ Capfile
@@ -12,7 +12,6 @@ require 'capistrano/rvm'
 require 'capistrano/bundler'
 require 'capistrano/rails/assets'
 require 'capistrano/rails/migrations'
-require 'capistrano/passenger/no_hook'
 
 
 # Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
--- config/unicorn/staging.rb
+++
@@ -1,7 +0,0 @@
-working_directory "/var/www/betaparticipa.podemos.info/current"
-pid "/var/www/betaparticipa.podemos.info/current/tmp/pids/unicorn.pid"
-stderr_path "/var/www/betaparticipa.podemos.info/shared/log/unicorn.log"
-stdout_path "/var/www/betaparticipa.podemos.info/shared/log/unicorn.log"
-listen "127.0.0.1:8080"
-worker_processes 2
-timeout 240
