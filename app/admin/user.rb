235a48a02b681c74b0474efe13b048e1e7e0759e
Leonardo Diez <leiodd@gmail.com>
2017-04-04 11:36:29 +0200

    [users] Ask gender.

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -21,7 +21,7 @@ ActiveAdmin.register User do
   scope :banned
   scope :verified
 
-  permit_params :email, :phone, :unconfirmed_phone, :password, :password_confirmation, :first_name, :last_name, :document_type, :document_vatid, :born_at, :address, :town, :postal_code, :province, :country, :vote_province, :vote_town, :wants_newsletter, :vote_district, :circle
+  permit_params :email, :phone, :unconfirmed_phone, :password, :password_confirmation, :first_name, :last_name, :gender, :document_type, :document_vatid, :born_at, :address, :town, :postal_code, :province, :country, :vote_province, :vote_town, :wants_newsletter, :vote_district, :circle
 
   index download_links: -> { current_user.is_admin? && current_user.superadmin? } do
     selectable_column
@@ -94,6 +94,9 @@ ActiveAdmin.register User do
       row :full_name
       row :first_name
       row :last_name
+      row :gender do
+        user.gender_name
+      end
       row :document_type do
         user.document_type_name
       end
@@ -233,6 +236,7 @@ ActiveAdmin.register User do
     column :id
     column :first_name
     column :last_name
+    column :gender_name
     column :document_vatid
     column :born_at
     column :email
