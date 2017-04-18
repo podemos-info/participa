892f8f9203fe19d249de467f4f47730cd758c924
Carles Muiños <carles.ml.dev@gmail.com>
2017-04-18 20:07:42 +0200

    [oauth2] Custom OAuth2 authorization views

--- vendor/overrides/bec/config/application.rb
+++ vendor/overrides/bec/config/application.rb
@@ -4,6 +4,9 @@ module Participa
     config.i18n.available_locales = ['es', 'ca']
     config.to_prepare do
       Devise::Mailer.layout "email"
+
+      # Authorization endpoint
+      Doorkeeper::AuthorizationsController.layout "doorkeeper/authorization"
     end
   end
 end
