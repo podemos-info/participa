4d88cfde43f72601aecb05446760e0d704762f59
Andrés Pereira <apereira@alabs.org>
2017-04-18 20:14:01 -0400

    [deploy] Fixing deploy with capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # config valid only for Capistrano 3.4.0
-lock '3.4.0'
+lock '3.4.1'
 
 set :application, 'participa.barcelonaencomu.cat'
 set :repo_url, 'https://github.com/GuanyemBarcelona/participa'
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/redis.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system db/podemos}
 
 set :user, 'participa'
