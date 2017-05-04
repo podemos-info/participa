25934dd5722c394a31086c213aefac5a464dc37d
Andrés Pereira <apereira@alabs.org>
2017-05-04 18:51:50 +0200

    Fixing SMTP with mailserver.yml config

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.4.1'
 
 set :application, 'participa.barcelonaencomu.cat'
 set :repo_url, 'https://github.com/GuanyemBarcelona/participa'
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/mailserver.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system db/podemos}
 
 set :user, 'participa'
