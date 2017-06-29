1e59dcf07754430004281a6ae183d929bd33f16d
ggalancs <ggalancs@podemos.info>
2017-06-29 10:17:44 +0200

    Modify Migration to allow file attachments

--- db/migrate/20170627141734_create_user_verifications.rb
+++ db/migrate/20170627141734_create_user_verifications.rb
@@ -1,12 +1,15 @@
 class CreateUserVerifications < ActiveRecord::Migration
   def change
     create_table :user_verifications do |t|
-      t.integer :user_id
-      t.integer :author_id
+      t.references :user, null: false
+      t.references :author_id,foreign_key:{to_table: :users}
       t.datetime :processed_at
       t.boolean :result
 
       t.timestamps null: false
     end
+
+    add_attachment :user_verifications, :front_vatid
+    add_attachment :user_verifications, :back_vatid
   end
 end
