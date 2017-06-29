6760c3b705cacd81005727ef641af61469ecfcf6
ggalancs <ggalancs@podemos.info>
2017-06-29 13:26:14 +0200

    Add Scopes and file directory to UserVerification Model

--- app/models/user_verification.rb
+++ app/models/user_verification.rb
@@ -1,2 +1,14 @@
 class UserVerification < ActiveRecord::Base
+  belongs to :user, -> { with_deleted }
+
+  validates :user, uniqueness: {scope: :user}, allow_blank: false, allow_nil: false
+
+  scope :needed, -> {where(result: nil)}
+  scope :passed, -> {where(result: true)}
+  scope :failed, -> {where(result: false)}
+
+  def files_folder
+    "#{Rails.application.root}/non-public/system/verification_vatid/#{id}/"
+  end
+
 end
