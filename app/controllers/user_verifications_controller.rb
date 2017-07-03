38ba420dc7485a66c712f5e8feb52af70a431691
Leonardo Diez <leiodd@gmail.com>
2017-07-03 10:25:39 +0200

    User verifications working.

--- app/controllers/user_verifications_controller.rb
+++ app/controllers/user_verifications_controller.rb
@@ -5,13 +5,11 @@ class UserVerificationsController < ApplicationController
   end
 
   def create
-    @user_verification = UserVerification.build(user_verification_params, user: current_user)
-    if @user_verification.valid?
-      @user_verification.save
+    @user_verification = UserVerification.new user_verification_params.merge(user: current_user)
+    if @user_verification.save
       redirect_to(create_vote_path(election_id: params[:election_id])) and return if params[:election_id]
       redirect_to(root_path, flash: { notice: "Ya hemos recibido tu documentación y la procesaremos proximamente." })
     else
-      flash.now[:error] = "Ha ocurrido un error al recibir tus ficheros, inténtalo nuevamente por favor."
       render :new
     end
   end
@@ -19,7 +17,7 @@ class UserVerificationsController < ApplicationController
   private
 
   def user_verification_params
-    params.require(:user_verification).permit(:procesed_at, :result)
+    params.require(:user_verification).permit(:procesed_at, :front_vatid, :back_vatid, :terms_of_service)
   end
 end
 
--- app/models/user_verification.rb
+++ app/models/user_verification.rb
@@ -1,17 +1,32 @@
 class UserVerification < ActiveRecord::Base
   belongs_to :user, -> { with_deleted }
 
-  has_attached_file :front_vatid
-  has_attached_file :back_vatid
+  has_attached_file :front_vatid, path: ":rails_root/non-public/system/:class/:attachment/:id_partition/:style/:filename", styles: { thumb: ["450x300#", :png] }
+  has_attached_file :back_vatid, path: ":rails_root/non-public/system/:class/:attachment/:id_partition/:style/:filename", styles: { thumb: ["450x300#", :png] }
 
-  validates :user, presence: true
+  validates :user, :front_vatid, presence: true
+  validates :back_vatid, presence: true, if: :require_back?
+  validates :terms_of_service, acceptance: true
+
+  validates_attachment_content_type :front_vatid, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :back_vatid, content_type: /\Aimage\/.*\z/
+
+  validates_attachment_size :front_vatid, :back_vatid, less_than: 1.megabyte
+
+  after_validation do
+    errors.each do |attr|
+      if attr.to_s.starts_with?("front_vatid_") || attr.to_s.starts_with?("back_vatid_")
+        errors.delete(attr)
+      end
+    end
+  end
 
   scope :pending, -> {where(result: nil)}
   scope :passed, -> {where(result: true)}
   scope :failed, -> {where(result: false)}
 
-  def files_folder
-    "#{Rails.application.root}/non-public/system/user_verifications/#{id_partition}/"
+  def require_back?
+    !user.is_passport?
   end
 
 end
