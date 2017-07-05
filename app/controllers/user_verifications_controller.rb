683612de4608f0f7b7c3efe06347cc693413b1a0
ggalancs <ggalancs@podemos.info>
2017-07-05 11:06:50 +0200

    Redirect to personal data to allow user review it before send the card

--- app/controllers/user_verifications_controller.rb
+++ app/controllers/user_verifications_controller.rb
@@ -1,6 +1,5 @@
 class UserVerificationsController < ApplicationController
   before_action :check_verified
-
   def new
     @user_verification = UserVerification.for current_user
   end
@@ -8,8 +7,13 @@ class UserVerificationsController < ApplicationController
   def create
     @user_verification = UserVerification.for current_user, user_verification_params
     if @user_verification.save
-      redirect_to(create_vote_path(election_id: params[:election_id])) and return if params[:election_id]
-      redirect_to(root_path, flash: { notice: "Ya hemos recibido tu documentación y la procesaremos proximamente." })
+      if  user_verification_params[:wants_card] == "1"
+        redirect_to(edit_user_registration_path ,flash: { notice: [t('podemos.user_verification.documentation_received'),
+                                                                   t('podemos.user_verification.please_check_details')].join("<br>")})
+      else
+        #redirect_to(create_vote_path(election_id: user_verification_params[:election_id])) and return if user_verification_params[:election_id]
+        redirect_to(root_path, flash: { notice: t('podemos.user_verification.documentation_received')})
+      end
     else
       render :new
     end
@@ -18,11 +22,12 @@ class UserVerificationsController < ApplicationController
   private
 
   def check_verified
-    redirect_to(root_path, flash: { notice: "Tu usuario ya está verificado" }) if current_user.verified?
+    redirect_to(root_path, flash: { notice: t('podemos.user_verification.user_already_verified') }) if current_user.verified?
   end
 
   def user_verification_params
-    params.require(:user_verification).permit(:procesed_at, :front_vatid, :back_vatid, :terms_of_service)
+    params.require(:user_verification).permit(:procesed_at, :front_vatid, :back_vatid, :terms_of_service, :wants_card)
   end
+
 end
 
