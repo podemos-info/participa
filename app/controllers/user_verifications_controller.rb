c26e332badd0f21041fa6d642279b6bf161030dc
Leonardo Diez <leiodd@gmail.com>
2017-07-06 17:36:36 +0200

    User verification text fixes.

--- app/controllers/user_verifications_controller.rb
+++ app/controllers/user_verifications_controller.rb
@@ -1,5 +1,6 @@
 class UserVerificationsController < ApplicationController
   before_action :check_valid_and_verified
+
   def new
     @user_verification = UserVerification.for current_user
   end
@@ -7,11 +8,11 @@ class UserVerificationsController < ApplicationController
   def create
     @user_verification = UserVerification.for current_user, user_verification_params
     if @user_verification.save
-      if  user_verification_params[:wants_card] == "1"
+      if params[:wants_card].present?
         redirect_to(edit_user_registration_path ,flash: { notice: [t('podemos.user_verification.documentation_received'),
                                                                    t('podemos.user_verification.please_check_details')].join("<br>")})
       else
-        #redirect_to(create_vote_path(election_id: user_verification_params[:election_id])) and return if user_verification_params[:election_id]
+        redirect_to(create_vote_path(election_id: params[:election_id])) and return if params[:election_id]
         redirect_to(root_path, flash: { notice: t('podemos.user_verification.documentation_received')})
       end
     else
@@ -22,21 +23,12 @@ class UserVerificationsController < ApplicationController
   private
 
   def check_valid_and_verified
-    invalid = true
-    Election.future.each do |e|
-      if e.has_valid_location_for? current_user
-        invalid = false
-        break
-      end
-    end
-
-    redirect_to(root_path, flash: { notice: t('podemos.user_verification.user_not_valid_to_verify') }) if invalid
+    redirect_to(root_path, flash: { notice: t('podemos.user_verification.user_not_valid_to_verify') }) if Election.future.none? { |e| e.has_valid_location_for? current_user }
     redirect_to(root_path, flash: { notice: t('podemos.user_verification.user_already_verified') }) if current_user.verified?
   end
 
   def user_verification_params
     params.require(:user_verification).permit(:procesed_at, :front_vatid, :back_vatid, :terms_of_service, :wants_card)
   end
-
 end
 
