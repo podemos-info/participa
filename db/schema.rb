39f6d427b1e2895fd3490683d2f7b2f33ffabcb5
Gabriel Galán <ggalancs@yahoo.com>
2018-10-24 16:04:19 +0200

    first commit to add Iban account to microcredit_loans

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180914153505) do
+ActiveRecord::Schema.define(version: 20181024121106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -387,6 +387,7 @@ ActiveRecord::Schema.define(version: 20180914153505) do
     t.datetime "discarded_at"
     t.datetime "returned_at"
     t.integer  "transferred_to_id"
+    t.string   "iban_account"
   end
 
   add_index "microcredit_loans", ["document_vatid"], name: "index_microcredit_loans_on_document_vatid", using: :btree
