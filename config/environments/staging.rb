ead55a53e15085438e48a5b4f8098924c49baf4f
Andrés Pereira de Lucena <apereira@alabs.org>
2018-12-13 16:37:26 +0100

    Removes SMTP for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -80,9 +80,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # FIXME: AWS::SES::ResponseError: Throttling - Daily message quota exceeded.
-  config.action_mailer.delivery_method = :ses
-  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :file
 
 #  ActionMailer::Base.smtp_settings = {
 #    :address              => Rails.application.secrets.smtp["address"],
