029ccbb76b28054732151ef89cd62d4851ef72a5
Gabriel Galán <ggalancs@yahoo.com>
2019-01-10 16:46:41 +0100

    view the corresponding Microcredit_options of each radio button and allow relationships in model microcredit_option

--- app/models/microcredit_option.rb
+++ app/models/microcredit_option.rb
@@ -1,7 +1,6 @@
 class MicrocreditOption < ActiveRecord::Base
   belongs_to :microcredit
-
+  belongs_to :parent, class_name: "MicrocreditOption"
+  has_many :children, foreign_key: :parent_id, class_name: "MicrocreditOption", inverse_of: :parent
   validates :name, presence: true
-
-
 end
