b835bc3dc7e5494901aa06d910c660cfdb4825fa
leio10 <leiodd@gmail.com>
2019-11-29 13:27:00 +0100

    Add audio captchas (#39)
    * Add audio captchas.
    
    * Fixes.

---
+++ app/controllers/audio_captcha_controller.rb
@@ -0,0 +1,46 @@
+class AudioCaptchaController < ApplicationController
+  LETTERS = {
+    "A"=>"A", "B"=>"Be", "C"=>"Ce", "D"=>"De", "E"=>"E", "F"=>"Efe", "G"=>"Ge", "H"=>"Hache", "I"=>"I", "J"=>"Jota", "K"=>"Ka", "L"=>"Ele", "M"=>"Eme", "N"=>"Ene",
+    "O"=>"O", "P"=>"Pe", "Q"=>"Cu", "R"=>"Erre", "S"=>"Ese", "T"=>"Te", "U"=>"U", "V"=>"Uve", "W"=>"Uve doble", "X"=>"Equis", "Y"=>"Y griega", "Z"=>"Zeta"
+  }.freeze
+
+  def index
+    FileUtils.mkdir_p file_dir
+
+    speech.save file_path
+
+    send_file file_path, type: 'audio/mp3', disposition: :inline
+  end
+
+  private
+
+  def speech
+    @speech ||= ESpeak::Speech.new(
+      captcha_value_spelling,
+      voice: "es+#{Random.rand(2) > 0 ? 'f' : 'm'}#{Random.rand(4)+1}",
+      speed: 90 + Random.rand(40),
+      pitch: Random.rand(30),
+      capital: Random.rand(30) + 3
+    )
+  end
+
+  def captcha_value_spelling
+    @captcha_value_spelling ||= captcha_value.chars.map {|letter| I18n.t("simple_captcha.letters.#{letter}")} .join " "
+  end
+
+  def captcha_value
+    @captcha_value ||= SimpleCaptcha::Utils::simple_captcha_value(captcha_key)
+  end
+
+  def captcha_key
+    @captcha_key ||= params[:captcha_key]
+  end
+
+  def file_path
+    @file_path ||= "#{file_dir}/#{captcha_key}.mp3"
+  end
+
+  def file_dir
+    @file_dir ||= "#{Rails.root}/tmp/audios"
+  end
+end
