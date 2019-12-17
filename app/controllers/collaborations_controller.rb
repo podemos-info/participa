24fc112dde377cfe6e3973b3c8f111f1818c4de7
Gabriel Galán <ggalancs@yahoo.com>
2019-12-17 19:27:28 +0100

    Fix error division by zero

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -110,9 +110,7 @@ class CollaborationsController < ApplicationController
     @collaboration = force_single? ? current_user.single_collaboration : current_user.recurrent_collaboration
     return unless @collaboration
     start_date = [@collaboration.created_at.to_date, Date.today - 6.months].max
-    max = 0
-    max = 12/(@collaboration.frequency-1) if @collaboration.frequency > 0
-    @orders = @collaboration.get_orders(start_date, start_date + 12.months)[0..max]
+    @orders = @collaboration.get_orders(start_date, start_date + 12.months)[0..@collaboration.frequency == 0? 0 : (12/@collaboration.frequency) - 1]
     @order = @orders[0][-1]
   end
 
