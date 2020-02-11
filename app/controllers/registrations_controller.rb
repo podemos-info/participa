c7eefe65dad39fb986d2e8541dbbd8fa881c2f85
Gabriel Galán <ggalancs@yahoo.com>
2020-02-11 13:47:17 +0100

    Allow to update wants_info_by_sms field

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -105,7 +105,7 @@ class RegistrationsController < Devise::RegistrationsController
     fields = %w[email password password_confirmation current_password gender address postal_code country province town]
     fields += %w[vote_province vote_town] if current_user.can_change_vote_location?
     fields += %w[first_name last_name born_at] unless locked_personal_data?
-
+    fields += %w[wants_information_by_sms]
     params.require(:user).permit(*fields)
   end
 
