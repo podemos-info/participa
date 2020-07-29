edfc7428e4f52ee7d2455b387be424eb0ada784b
Gabriel Galán <ggalancs@yahoo.com>
2020-07-29 15:37:12 +0200

    remove unnecesary field

--- db/migrate/20200729112226_add_user_id_to_circle.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToCircle < ActiveRecord::Migration
-  def change
-    add_column :circles, :user_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200729112226) do
+ActiveRecord::Schema.define(version: 20200625113025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,6 @@ ActiveRecord::Schema.define(version: 20200729112226) do
     t.string   "original_code"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.integer  "user_id"
   end
 
   create_table "collaborations", force: :cascade do |t|
