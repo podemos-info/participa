db24dfe950e3fe7c40d13b67836687f58bbd4000
Gabriel Galán <ggalancs@yahoo.com>
2020-08-12 15:05:02 +0200

    Add option to upload circles from file in admin circles

--- app/admin/circle.rb
+++ app/admin/circle.rb
@@ -1,7 +1,17 @@
 ActiveAdmin.register Circle do
   menu :parent => "Users"
   #permit_params :original_code, :original_name
-
+  sidebar "Añadir Circulos desde fichero", 'data-panel' => :collapsed, :only => :index, priority: 1 do
+    render('upload_circles')
+  end
   index download_links: -> { current_user.is_admin? && current_user.superadmin? }
 
+
+  collection_action :upload_circles, :method => :post do
+    file_input = params["circles"]["file"].tempfile
+    CSV.foreach(file_input, :headers => true, :col_sep=> "\t", encoding: "UTF-8") do |row|
+      Circle.create(row.to_hash)
+    end
+    redirect_to collection_path, notice: "¡Fichero importado correctamente!"
+  end
 end
