436eacf3020ebead4465566e9a6653625fedcd1b
Gabriel Galán <ggalancs@yahoo.com>
2020-09-16 16:12:42 +0200

    Fix unnecessary calls to still_militant? function and avoid unnecessary db writing

--- app/models/election.rb
+++ app/models/election.rb
@@ -93,7 +93,7 @@ class Election < ActiveRecord::Base
         when 3 then user.has_vote_town? && valid_locations.any? {|l| l.location == user.vote_town_numeric}
         when 4 then user.has_vote_town? && valid_locations.any? {|l| l.location == user.vote_island_numeric}
         when 5 then user.country!="ES" && valid_locations.any?
-        when 6 then user.still_militant? && user.vote_circle.present? && valid_locations.any? {|l| l.location == user.vote_circle_id.to_s}
+        when 6 then user.vote_circle.present? && valid_locations.any? {|l| l.location == user.vote_circle_id.to_s} && user.militant?
       end
     end
   end
