e2f289d35194c18a0743a32658f8d9a1c6da0aa8
Gabriel Galán <ggalancs@yahoo.com>
2020-11-24 16:57:05 +0100

    create scopes for users in exterior and users in spain

--- app/models/user.rb
+++ app/models/user.rb
@@ -187,6 +187,8 @@ class User < ActiveRecord::Base
   scope :has_vote_circle, -> { where("vote_circle_id IS NOT NULL") }
   scope :wants_information_by_sms, -> {where(wants_information_by_sms: true)}
   scope :militant_and_exempt_from_payment, -> {militant.exempt_from_payment}
+  scope :exterior, -> { where.not(country: "ES") }
+  scope :spain, -> { where(country: "ES") }
 
   ransacker :vote_province, formatter: proc { |value|
     values = value.split(",")
