44196fbfb99a314eab23b8aa3dd409c31dcf7634
Gabriel Galán <ggalancs@yahoo.com>
2020-11-26 11:40:33 +0100

    rename the cookie to be more standard

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_podemos-juntos_session'
+Rails.application.config.session_store :cookie_store, key: '_podemos_participa_session'
