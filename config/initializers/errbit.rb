81a1eeda1387d6081a49697efde1ef671d140b65
Gabriel Galán <ggalancs@yahoo.com>
2020-11-26 11:36:58 +0100

    update airbrake configuration to make it compatible with rails 5.0

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,10 +1,3 @@
-Airbrake.configure do |config|
-  config.api_key = Rails.application.secrets.airbrake["api_key"]
-  config.host    = Rails.application.secrets.airbrake["host"]
-  config.port    = Rails.application.secrets.airbrake["port"]
-  config.secure  = config.port == 443
-end
-
 module Airbrake
   def self.notify_or_raise ex
     if self.configuration.api_key
