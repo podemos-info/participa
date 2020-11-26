1e044c8895f68e27833fa02cf62e29017827fc6c
Gabriel Galán <ggalancs@yahoo.com>
2020-11-26 11:44:52 +0100

    update rails_helper.rb file to make it compatible with rails 5.0

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,7 +24,7 @@ require 'rspec/rails'
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
+ActiveRecord::Migration[4.2].maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
