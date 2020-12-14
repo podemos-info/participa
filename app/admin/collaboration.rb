949ba8037d23e022e529b2f00400d1dadcf73e77
Gabriel Galán <ggalancs@yahoo.com>
2020-12-14 15:27:57 +0100

    add order reports by circle and by circle_and_cp in admin collaborations page

--- app/admin/collaboration.rb
+++ app/admin/collaboration.rb
@@ -825,7 +825,7 @@ ActiveAdmin.register Collaboration do
     # ----------------------- Generate Postal Code data ---------------------------------------------------------------------------
 
     towns_data = Hash.new {|h,k| h[k] = Hash.new{|h,k| h[k] = Hash.new{|h,k| h[k] = 0}}}
-    query = Order.paid.joins("LEFT JOIN users on orders.user_id = users.id").where("orders.town_code is not null and orders.amount > 0").where("users.vote_circle_id is not null").group(:town_code,:postal_code, Order.unique_month('payable_at')).order(:town_code, "users.postal_code", Order.unique_month('payable_at')).pluck('town_code', 'postal_code', Order.unique_month('payable_at'), 'count(orders.id) as count_id, sum(orders.amount) as sum_amount, users.postal_code as pc')
+    query = Order.paid.joins("LEFT JOIN users on orders.user_id = users.id").where("orders.town_code is not null and orders.amount > 0").where("users.vote_circle_id is null").group(:town_code,:postal_code, Order.unique_month('payable_at')).order(:town_code, "users.postal_code", Order.unique_month('payable_at')).pluck('town_code', 'postal_code', Order.unique_month('payable_at'), 'count(orders.id) as count_id, sum(orders.amount) as sum_amount, users.postal_code as pc')
     query.each do|c,cp,m,t,v|
       num_month = m.to_i
       if (towns_data[c][cp][num_month] == 0)
