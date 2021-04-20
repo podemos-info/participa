c9f7dda5d0e76317a82766d3419bdc3ed8f3b268
Joao Maria Arranz <malka@localhost.localdomain>
2021-04-20 13:00:03 +0200

    Primer test de prueba funcionando

--- features/home_page.feature
+++ features/home_page.feature
@@ -3,4 +3,9 @@ Feature: Home page
 
   Scenario: Viewing application's home page
     Given I am on the homepage
-    Then I should see the "My first" post
+    Then I should see the "Bienvenido/a al Portal de Participación de Podemos." text
+
+   Scenario: Login user as test user
+     Given there area a test user
+     When I fill login form
+     Then I Should see "Herramientas de participación ciudadana"
--- features/step_definitions/home_page_steps.rb
+++ features/step_definitions/home_page_steps.rb
@@ -8,3 +8,21 @@ Then(/^I should see the "(.*?)" /) do |title|
 
   page.should have_content(title)
 end
+
+Given('there area a test user') do
+  @user = FactoryBot.create(:user)
+  
+end
+
+When('I fill login form') do
+  visit("/")
+  within("#new_user") do
+    fill_in 'user[login]', with: 'foo1@example.com'
+    fill_in 'user[password]', with: '123456789'
+  end
+  click_button 'Iniciar sesión'
+end
+
+Then('I Should see {string}') do |string|
+  page.should have_content(string)
+end
