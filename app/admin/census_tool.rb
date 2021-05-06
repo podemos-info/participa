33c1e7cf2693546dee136236c6a2866ded4984e3
Gabriel Galán <ggalancs@yahoo.com>
2021-05-06 18:17:48 +0200

    move qr_scanner from paper_vote.html.erb to census_tool.rb

--- app/admin/census_tool.rb
+++ app/admin/census_tool.rb
@@ -2,15 +2,17 @@ ActiveAdmin.register_page "CensusTool" do
   menu :parent => "Users"
   content title:"Herramienta de control de Censo" do
     panel "" do
-      render "admin/census_tool/census_tool", width: "100%"
+      render partial: 'admin/census_tool/qr_scanner'
+      render partial: "admin/census_tool/census_tool" #, layout:false, width: "100%"
     end
   end
 
   page_action :search_document_vatid, method: [:post] do
     dt = params["document_type"]
     dn = params["document_vatid"]
+    qr_hash = params["user_qr_hash"]
     paper_vote_user = User.confirmed.not_banned.militant.where(vote_circle_id:current_user.vote_circle_id).where("lower(document_vatid) = ?", dn.downcase).find_by(document_type: dt)
-    if paper_vote_user
+    if paper_vote_user && check_verified_user_hash(dn,qr_hash)
       message= { notice: "#{paper_vote_user.first_name}, con #{paper_vote_user.document_type_name} #{paper_vote_user.document_vatid}, puede participar." }
 
     else
@@ -19,5 +21,11 @@ ActiveAdmin.register_page "CensusTool" do
     end
     redirect_to admin_censustool_path, flash: message
 
+  end
+  controller do
+    def check_verified_user_hash(document_vatid,received_hash)
+      user = User.find_by_document_vatid(document_vatid)
+      user ? user.is_qr_hash_correct?(received_hash) : false
+    end
   end
 end
