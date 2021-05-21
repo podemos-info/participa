121cad59a5035790b6b81caf5fb32dfaae3e23b5
Gabriel Galán <ggalancs@yahoo.com>
2021-05-21 13:46:23 +0200

    upgrade gem for aws emails only used at staging environment

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,4 +1,4 @@
-ActionMailer::Base.add_delivery_method(:ses,
+ActionMailer::Base.add_action_mailer_delivery_method(:ses,
   credentials: Aws::Credentials.new(
     Rails.application.secrets.aws_ses["access_key_id"],
     Rails.application.secrets.aws_ses["secret_access_key"]
