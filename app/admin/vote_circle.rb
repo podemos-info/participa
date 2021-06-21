06f6e942239ff92170f20509430a160e35f6fec8
Gabriel Galán <ggalancs@yahoo.com>
2021-06-21 10:49:24 +0200

    add assign_vote_circle_territory method to vote_circle

--- app/admin/vote_circle.rb
+++ app/admin/vote_circle.rb
@@ -116,8 +116,63 @@ ActiveAdmin.register VoteCircle do
       end
     end
 
+    def assign_vote_circle_territory(vc)
+      internal_type = 0
+      neighborhood_type = 1
+      town_type = 2
+      region_type = 3
+      exterior_type = 4
+
+      spain_types = [["TB%",neighborhood_type],["TM%",town_type],["TC%",region_type]]
+      internal = ["IP%", internal_type]
+      known_types  = ["TB%", "TM%", "TC%", "IP%"]
+      spain_code ="ES"
+
+      vc.kind = type_code
+      if vc.town.present?
+        town_code = vc.town
+        province_code = "p_#{vc.town[2,2]}"
+        autonomy_code = Podemos::GeoExtra::AUTONOMIES[province_code][0]
+        island = Podemos::GeoExtra::ISLANDS[vc.town]
+        island_code = vc.island_code
+        island_code = island.present? ? island[0] : nil unless island_code.present?
+        country_code = spain_code
+      else
+        if vc.code_in_spain?
+          town_code = nil
+          autonomy_code = "c_#{vc.code[2,2]}"
+          province_code = "p_#{vc.code[4,2]}"
+          island_code = vc.island_code
+          country_code = spain_code
+        else
+          town_code = nil
+          province_code = nil
+          autonomy_code = nil
+          island_code = nil
+          country_code = vc.code[0,2].upcase
+        end
+      end
+      vc.town = town_code
+      vc.province_code = province_code
+      vc.autonomy_code = autonomy_code
+      vc.island_code = island_code
+      vc.country_code = country_code
+
+      exterior_circles = VoteCircle.all.where("code not like all(array[?])",known_types).where(country_code:nil, autonomy_code: nil,province_code: nil)
+      exterior_circles.find_each do |vc|
+        vc.kind = exterior_type
+        vc.town = nil
+        vc.province_code = nil
+        vc.autonomy_code = nil
+        vc.island_code = nil
+        vc.country_code = vc.code[0,2].upcase
+        vc.save!
+      end
+    end
+
     def assign_vote_circle_code(resource)
       resource.code = resource.original_code if resource.code.nil?
+      assign_vote_circle_territory resource
     end
   end
 
